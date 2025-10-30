47fe9541130cb5b45220f2d1611228872926cee3
unknown <juliacamposguima@gmail.com>
2025-10-30 07:58:32 -0300

    trabalho

--- trabalho.py
+++ trabalho.py
@@ -55,7 +55,7 @@ sns.regplot(x="População", y="Valor", data=df, ci=None, line_kws={'color':'red
 plt.title("Relação entre População e Despesa com Segurança")
 plt.show()
 
-###
+### Testes 
 
 df= pd.read_csv("C:\\Documentos\\analise_dados\\dados_completos (1).csv")
 df
@@ -111,3 +111,50 @@ modelo = sm.OLS(y, X).fit()
 
 # Exibir o resumo dos resultados
 print(modelo.summary())
+
+
+###
+
+import pandas as pd
+import seaborn as sns
+import statsmodels.api as sm 
+import matplotlib.pyplot as plt
+
+# Importar os dados
+df = pd.read_excel("C:\\Documentos\\analise_dados\\dados_completos_agrupados_com_impostos.xlsx")
+
+# Verificar estrutura dos dados
+df.shape
+df.columns
+df
+
+# Variável independente (X)
+X = df["Impostos_Arrecadados"]
+
+# Variável dependente (y)
+y = df["Qtd_Homicidios"]
+
+# Adicionar constante (intercepto)
+X = sm.add_constant(X)
+
+# Estimar o modelo MQO (OLS)
+modelo = sm.OLS(y, X).fit()
+
+# Exibir o resumo dos resultados
+print(modelo.summary())
+
+# Variável independente (X)
+X = df["Impostos_Arrecadados"]
+
+# Variável dependente (y)
+y = df["Gasto_Seguranca"]
+
+# Adicionar constante (intercepto)
+X = sm.add_constant(X)
+
+# Estimar o modelo MQO (OLS)
+modelo = sm.OLS(y, X).fit()
+
+# Exibir o resumo dos resultados
+print(modelo.summary())
+
